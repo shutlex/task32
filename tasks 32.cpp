dc7091167353a4b4633a8434e7f24923fa0e0c3b
Oleksandr <sprokhorenko0609@gmail.com>
2024-12-20 11:53:01 +0200

    first commit

--- tasks 32.cpp
+++ tasks 32.cpp
@@ -5,8 +5,6 @@
 
 void Thread1(HANDLE hEvent) {
     std::cout << "Thread1: Waiting for event...\n";
-
-    // Wait for the event to be signaled
     DWORD dwWaitResult = WaitForSingleObject(hEvent, INFINITE);
 
     if (dwWaitResult == WAIT_OBJECT_0) {
@@ -18,22 +16,19 @@ void Thread1(HANDLE hEvent) {
 }
 
 void Thread2(HANDLE hEvent) {
-    // Delay before signaling the event (e.g., 3 seconds)
     std::this_thread::sleep_for(std::chrono::seconds(3));
 
     std::cout << "Thread2: Signaling the event...\n";
 
-    // Signal the event
     SetEvent(hEvent);
 }
 
 int main() {
-    // Create an event (manual reset, initially unsignaled)
     HANDLE hEvent = CreateEvent(
-        NULL,           // Security attributes
-        FALSE,          // Auto-reset event (FALSE means it does not reset automatically)
-        FALSE,          // Initial state of the event (unsignaled)
-        NULL            // Event name (NULL means unnamed)
+        NULL,          
+        FALSE,         
+        FALSE,          
+        NULL            
     );
 
     if (hEvent == NULL) {
@@ -41,15 +36,12 @@ int main() {
         return 1;
     }
 
-    // Create two threads
     std::thread t1(Thread1, hEvent);
     std::thread t2(Thread2, hEvent);
 
-    // Wait for threads to finish
     t1.join();
     t2.join();
 
-    // Close the event handle
     CloseHandle(hEvent);
 
     return 0;
